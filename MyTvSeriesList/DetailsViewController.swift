092b3f7e291b32de990dada1a4a42493a30d8d3e
Burak Afyonlu <burakafyonlu0280@gmail.com>
2023-01-19 21:30:41 +0300

    second commit
    to changes

---
+++ MyTvSeriesList/DetailsViewController.swift
@@ -0,0 +1,29 @@
+//
+//  DetailsViewController.swift
+//  MyTvSeriesList
+//
+//  Created by Burak Afyonlu on 19.01.2023.
+//
+
+import UIKit
+
+class DetailsViewController: UIViewController {
+
+    @IBOutlet weak var nameText: UITextField!
+    
+    @IBOutlet weak var imageView: UIImageView!
+    @IBOutlet weak var seasonText: UITextField!
+    @IBOutlet weak var imdbText: UITextField!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        
+        
+    }
+    
+
+    @IBAction func saveClicked(_ sender: Any) {
+    }
+    
+}
--- MyTvSeriesList/ViewController.swift
+++ MyTvSeriesList/ViewController.swift
@@ -12,6 +12,8 @@ class ViewController: UIViewController ,UITableViewDelegate , UITableViewDataSou
 
     @IBOutlet weak var tableView: UITableView!
     
+    var nameArray = [String]()
+    var idArray = [UUID]()
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -19,15 +21,64 @@ class ViewController: UIViewController ,UITableViewDelegate , UITableViewDataSou
         tableView.delegate = self
         tableView.dataSource = self
     
+        navigationController?.navigationBar.topItem?.rightBarButtonItem = UIBarButtonItem(barButtonSystemItem: UIBarButtonItem.SystemItem.add, target: self, action: #selector(addButton))
         
-        
+        getData()
         
     }
     
-    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+    @objc func addButton() {
         
+        performSegue(withIdentifier: "toDetailsVC", sender: nil)
         
     }
+    
+    override func viewWillAppear(_ animated: Bool) {
+        
+        NotificationCenter.default.addObserver(self, selector: #selector(getData), name: NSNotification.Name("newData"), object: nil)
+    }
+    
+    @objc func getData() {
+        
+        nameArray.removeAll(keepingCapacity: false)
+        idArray.removeAll(keepingCapacity: false)
+        
+        let appDelegate = UIApplication.shared.delegate as! AppDelegate
+        let context = appDelegate.persistentContainer.viewContext
+        let fetchRequest = NSFetchRequest<NSFetchRequestResult>(entityName: "Series")
+        fetchRequest.returnsObjectsAsFaults = false
+        
+        do {
+        
+            let results = try context.fetch(fetchRequest)
+            for result in results as! [NSManagedObject] {
+                
+                if let name = result.value(forKey: "name") as? String {
+                    self.nameArray.append(name)
+                }
+                if let id = result.value(forKey: "id") as? UUID {
+                    self.idArray.append(id)
+                }
+                self.tableView.reloadData()
+            }
+        } catch { print("Error")
+    }
+        }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        
+        let cell = UITableViewCell()
+        cell.textLabel?.text = nameArray[indexPath.row]
+        return cell
+        
+    }
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        
+        return nameArray.count
+        
+    }
+    
 
 
 }
